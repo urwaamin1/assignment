f39354155762bac5527f48ad6d98964cc4a78119
unknown <urwaamin6959@gmail.com>
2024-09-09 21:55:17 +0500

    first commit

--- form/java.js
+++ form/java.js
@@ -1,3 +1,9 @@
-var a=inputfeild.getAttribute{
-    "data-my-attribute"
-}
+
+    document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById('demo').addEventListener('click', function() {
+            var username = document.getElementById('username').value;
+            var password = document.getElementById('password').value;
+            console.log('Username:', username);
+            console.log('Password:', password);
+        });
+    });
